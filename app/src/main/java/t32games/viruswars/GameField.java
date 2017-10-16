74c6e62b81a459193e472bc564edcea93a6a152d
RagingPython <SHOOTni1>
2017-10-16 14:03:54 +0300

    V+P initial commit

---
+++ app/src/main/java/t32games/viruswars/GameField.java
@@ -0,0 +1,17 @@
+package t32games.viruswars;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by nuzhdin on 16.10.2017.
+ */
+
+public class GameField extends View {
+    public GameField(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+}
